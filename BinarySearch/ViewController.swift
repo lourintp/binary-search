9fef78a11b02dd1ee279e76599f14aafa167e5f5
lourintp <thiago.lourin@objective.com.br>
2020-02-13 20:21:03 -0300

    Initial Commit

---
+++ BinarySearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BinarySearch
+//
+//  Created by Thiago Lourin on 13/02/20.
+//  Copyright © 2020 Lourin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
